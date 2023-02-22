7725d8a1bc15a95ebd187edea2ed8a3f5000ae87
PhoonThetPine <thetpine254@gmail.com>
2023-02-22 21:05:39 +0800

    v4

--- app/src/main/java/com/example/hearlall/Fragment/SignLanguage.java
+++ app/src/main/java/com/example/hearlall/Fragment/SignLanguage.java
@@ -1,66 +1,101 @@
 package com.example.hearlall.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
+import com.example.hearlall.MainMenuPage_Activity;
 import com.example.hearlall.R;
+import com.example.hearlall.SignLanguage.AI_signLanguage_recognition_Activity;
+import com.example.hearlall.SignLanguage.First_SignLanguage_Activity;
+import com.example.hearlall.SignLanguage.SearchText_Activity;
+import com.example.hearlall.SignLanguage.SignLanguage_Activity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignLanguage#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SignLanguage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+   private Button btn_frequentlyUsed, btn_numbers, btn_alphabets, btn_ai_signLanguage;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignLanguage() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignLanguage.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignLanguage newInstance(String param1, String param2) {
-        SignLanguage fragment = new SignLanguage();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Context mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_language, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sign_language, container, false);
+
+        mContext = getActivity();
+
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Inflate the layout for this fragment
+        return rootView;
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findViews(view);
+    }
+
+    private void findViews(View v) {
+
+        //ImageView
+        btn_frequentlyUsed = v.findViewById(R.id.btn_frequentlyUsed);
+        btn_numbers = v.findViewById(R.id.btn_numbers);
+        btn_alphabets = v.findViewById(R.id.btn_alphabets);
+        btn_ai_signLanguage = v.findViewById(R.id.btn_ai_signLanguage);
+
+        pageDirectories();
+
+    }
+
+    private void pageDirectories() {
+
+        btn_frequentlyUsed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SignLanguage_Activity.class);
+                intent.putExtra("Userchoice", 3);
+                startActivity(intent);
+            }
+        });
+
+        btn_numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SignLanguage_Activity.class);
+                intent.putExtra("Userchoice", 2);
+                startActivity(intent);
+            }
+        });
+
+        btn_alphabets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SignLanguage_Activity.class);
+                intent.putExtra("Userchoice", 1);
+                startActivity(intent);
+            }
+        });
+
+        btn_ai_signLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, AI_signLanguage_recognition_Activity.class));
+            }
+        });
+    }
+
+
 }
